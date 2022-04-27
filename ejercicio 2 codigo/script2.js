70a3ae82d60b7b21d1daaaec73aa0d4bc26d3096
EzequielRayas <zeky8@outlook.com>
2022-04-26 19:51:30 -0500

    feat: apuntes clase 1

---
+++ ejercicio 2 codigo/script2.js
@@ -0,0 +1,7 @@
+
+alert('Bienvenido a la calculadora de áreas y perímetros.');
+
+console.log('Elige una figura:');
+console.log('1. Rectángulo');
+console.log('2. Triangulo isósceles');
+console.log('3. Cuadrado');
